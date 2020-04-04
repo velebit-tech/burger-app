abfcf0f47cbc6dbb41ff89fbab6ed2f16874ac04
Mario Markovic <velebit.business@gmail.com>
2020-04-04 15:20:41 +0200

    SECTION 8: MADE FIRST BURGER YEAAAAAAAAAAAAH :D

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -1,12 +1,13 @@
 import React, { Component } from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
+import Burger from '../../components/Burger/Burger';
 
 class BurgerBuilder extends Component {
   render () {
     return (
       <Auxiliary>
-        <div>Burger with meat</div>
+        <Burger />
         <div>Burger controls</div>
       </Auxiliary>
     );
