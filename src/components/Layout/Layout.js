8cd288e639368376409d7f08fa77ecd1771cc145
Mario Markovic <velebit.business@gmail.com>
2020-04-06 13:39:23 +0200

    section 8: burger app: added toolbar, next adding logo

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,11 +1,12 @@
 import React from 'react';
 
 import Auxiliary from '../../hoc/Auxiliary';
+import Toolbar from '../Navigation/Toolbar/Toolbar';
 import classes from './Layout.css';
 
 const layout = props => (
   <Auxiliary>
-    <div>Toolbar, Navigation, ...</div>
+    <Toolbar />
     <main className={classes.MainContent}>
       {props.children}
     </main>
